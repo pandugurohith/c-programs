c0b7ce034200200aa1f955f4a57b0bde6c18f8b2
pandugurohith <pandugurohith8@gmail.com>
2026-01-30 10:26:36 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a,b,c;
+  cout<<"Welcome to c++"<<endl;
+    cout<<"Enter a value: "<<endl;
+    cin>>a;
+    cout<<"Enter b value:  "<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"sum of two values: "<<c;
+    return 0;
+}
+
+ 
